94d1e522ab587a8d52ef6ff1d8aa3250d776c3d9
Zhipeng <zhipeng130@gmail.com>
2022-04-19 09:13:31 +0200

    Modification -ZP

--- FOODTIPAPP/app/src/main/java/com/example/foodtip/Controller/MisFavoritos.java
+++ FOODTIPAPP/app/src/main/java/com/example/foodtip/Controller/MisFavoritos.java
@@ -2,7 +2,7 @@ package com.example.foodtip.Controller;
 
 public class MisFavoritos extends Receptas {
 
-    public MisFavoritos(int id, String description, String title, CollectionImages images, Ingredients ingredients, Steps steps, Likes num_like, Comentaris comentaris) {
+    public MisFavoritos(String description, String title, CollectionImages images, Ingredients ingredients, Steps steps, Likes num_like, Comentaris comentaris) {
         super(description, title, images, ingredients, steps, num_like, comentaris);
     }
 }
--- FOODTIPAPP/app/src/main/java/com/example/foodtip/Controller/MisReceptas.java
+++ FOODTIPAPP/app/src/main/java/com/example/foodtip/Controller/MisReceptas.java
@@ -1,7 +1,7 @@
 package com.example.foodtip.Controller;
 
 public class MisReceptas extends Receptas{
-    public MisReceptas(int id, String description, String title, CollectionImages images, Ingredients ingredients, Steps steps, Likes num_like, Comentaris comentaris) {
+    public MisReceptas(String description, String title, CollectionImages images, Ingredients ingredients, Steps steps, Likes num_like, Comentaris comentaris) {
         super(description, title, images, ingredients, steps, num_like, comentaris);
     }
 }
