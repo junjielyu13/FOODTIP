665f0dcc8bd09933b799d8e543c9b3a12e5171e8
zhihan <zhihanlin29@gmail.com>
2022-04-20 20:57:00 +0200

    register

--- FOODTIPAPP/app/src/main/java/com/example/foodtip/View/MainActivity.java
+++ FOODTIPAPP/app/src/main/java/com/example/foodtip/View/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.foodtip.View;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,77 +19,67 @@ import com.example.foodtip.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Context context;
     public static FoodTip foodTip;
     public static Login_dades ld;
     private EditText userName;
     private EditText password;
-    private Button login_but;
-    private Button regi_but;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = getBaseContext();
         ld = new Login_dades();
         setContentView(R.layout.activity_login);
 
         userName = findViewById(R.id.UserNameEditTextLogIn);
         password = findViewById(R.id.PasswordEditTextLogIn);
-        login_but = findViewById(R.id.loginButton);
-        regi_but = findViewById(R.id.RegisterButton);
 
-        setLogInButtonsOnClickListeners();
-        setRegisterButtonsOnClickListeners();
     }
 
-    private void setRegisterButtonsOnClickListeners() {
-        regi_but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userName.getText().clear();
-                password.getText().clear();
-                startActivity(new Intent(context, RegisterActivity.class));
-            }
-        });
+    public void RegisterButtonOnClickListener(View view) {
+        userName.getText().clear();
+        password.getText().clear();
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 
-    private void setLogInButtonsOnClickListeners() {
-
-        login_but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = userName.getText().toString();
-                String pwd = password.getText().toString();
-                User user = ld.getUsersMap().getOrDefault(username, null);
-
-                if (user != null) {
-                    if (pwd.equals(user.getPassword())) {
-                        Toast.makeText(context, "Log In sucessful", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(context, HomePageActivity.class));
-                    } else {
-                        userName.getText().clear();
-                        password.getText().clear();
-                        AlertDialog alertDialog = new AlertDialog.Builder(context)
-                                .setTitle("Error!")
-                                .setMessage("Wrong password")
-                                .setIcon(R.mipmap.empty_img)
-                                .create();
-                        alertDialog.show();
-                    }
-                }else{
-                    userName.getText().clear();
-                    password.getText().clear();
-                    AlertDialog alertDialog = new AlertDialog.Builder(context)
-                            .setTitle("Error!")
-                            .setMessage("Username does not exist")
-                            .setIcon(R.mipmap.empty_img)
-                            .create();
-                    alertDialog.show();
+    public void LogInButtonOnClickListener(View view) {
+        final int minim_pwd_length = 8;
+        final int max_pwd_length = 16;
+        String username = userName.getText().toString();
+        String pwd = password.getText().toString();
+        User user = ld.getUsersMap().getOrDefault(username, null);
+        if (user != null) {
+            if (pwd.length() > max_pwd_length && pwd.length() < minim_pwd_length) {
+                showAlertDialog("Error!","Password length must be more than 8 characters or less equal than 16 characters",R.mipmap.ic_launcher);
+            } else {
+                if (pwd.equals(user.getPassword())) {
+                    Toast.makeText(this, "Log In sucessful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, HomePageActivity.class));
+                } else {
+                    showAlertDialog("Error!","Wrong password",R.mipmap.empty_img);
                 }
             }
-        });
+        } else {
+            showAlertDialog("Error","Username does not exist",R.mipmap.empty_img);
+
+        }
     }
 
+    private void showAlertDialog(String title, String message, int icon){
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setIcon(icon)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        userName.getText().clear();
+                        password.getText().clear();
+                    }
+                })
+                .create();
+        alertDialog.show();
+    }
 }
--- FOODTIPAPP/app/src/main/java/com/example/foodtip/View/RegisterActivity.java
+++ FOODTIPAPP/app/src/main/java/com/example/foodtip/View/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodtip.View;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +21,6 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private EditText repeatPassword;
-    private Button registerBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,21 +30,49 @@ public class RegisterActivity extends AppCompatActivity {
         username = findViewById(R.id.UserNameREGISTER);
         password = findViewById(R.id.PasswordREGISTER);
         repeatPassword = findViewById(R.id.RepeatPasswordREGISTER);
-        registerBtn = findViewById(R.id.registerButtonREGISTER);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user_name = username.getText().toString();
-                String pwd = password.getText().toString();
-                String rePwd = repeatPassword.getText().toString();
 
-                if((!MainActivity.ld.getUsersMap().containsKey(user_name)) && pwd.equals(rePwd)){
-                    MainActivity.ld.addUsers(user_name,pwd);
-                    Toast.makeText(getApplicationContext(), "Register Sucessful!", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            }
-        });
     }
 
+    public void RegisterButtonOnClickListenerRegister(View view) {
+        final int minim_pwd_length = 8;
+        final int max_pwd_length = 16;
+        String user_name = username.getText().toString();
+        String pwd = password.getText().toString();
+        String rePwd = repeatPassword.getText().toString();
+        if (MainActivity.ld.getUsersMap().containsKey(user_name)) {
+            showAlertDialog("Error!,", "Username exist!", R.mipmap.empty_img);
+        } else {
+            if (pwd.length() > max_pwd_length && pwd.length() < minim_pwd_length) {
+                showAlertDialog("Error!", "Password length must be more than 8 characters or less equal than 16 characters", R.mipmap.ic_launcher);
+            } else {
+                if (pwd.equals(rePwd)) {
+                    MainActivity.ld.addUsers(user_name, pwd);
+                    Toast.makeText(getApplicationContext(), "Register Sucessful!", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    showAlertDialog("Error!", "The repeat password is not the same as the previous one", R.mipmap.empty_img);
+                }
+            }
+        }
+
+
+    }
+
+    private void showAlertDialog(String title, String message, int icon) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setIcon(icon)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        username.getText().clear();
+                        password.getText().clear();
+                        repeatPassword.getText().clear();
+                    }
+                })
+                .create();
+        alertDialog.show();
+    }
 }
